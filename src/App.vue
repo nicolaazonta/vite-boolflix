0c43f0b748218d68c10c5443e937967fdefcae62
nicolaazonta <nicolazonta114@gmail.com>
2023-04-19 12:20:21 +0200

    create main components

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,32 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import { store } from './store'
+import SiteHeader from './components/SiteHeader.vue'
+import SiteMain from './components/SiteMain.vue'
+import SiteFooter from './components/SiteFooter.vue'
+
+export default {
+  components: {
+    SiteHeader,
+    SiteMain,
+    SiteFooter
+  },
+  data(){
+    return{
+      store
+    }
+  } 
+  
+}
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <SiteHeader></SiteHeader>
+  <SiteMain></SiteMain>
+  <SiteFooter></SiteFooter>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
+<style lang="scss" scoped>
+
 </style>
