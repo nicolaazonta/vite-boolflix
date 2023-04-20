0a19f4f4c21b9460b0086cc89730c0eb9b54b299
nicolaazonta <nicolazonta114@gmail.com>
2023-04-20 16:51:26 +0200

    apply transition on hover

--- src/components/SiteMain.vue
+++ src/components/SiteMain.vue
@@ -17,28 +17,28 @@ export default {
     <main>
         <div class="container">
             <div class="row">
-                <div class="col-4 py-3" v-for="movie in store.movies">
+                <div class="col-3 py-3" v-for="movie in store.movies">
                     <div id="movie" class="card bg-dark text-light h-100 p-2"><!-- movie -->
                         <img class="card-img h-100" :src="`https://image.tmdb.org/t/p/w342/${movie.poster_path}`" alt="">
                         <div class="card-img-overlay d-flex flex-column justify-content-end">
                             <h3 class="card-text">{{ movie.title }}</h3>
-                            <h5 class="card-text">original title: {{ movie.original_title }}</h5>
-                            <h5 class="card-text">original language: {{ movie.original_language }}</h5>
-                            <h5 class="card-text">average vote: {{ movie.vote_average }}</h5>
+                            <h5 class="card-text">{{ movie.original_title }}</h5>
+                            <h5 class="card-text">{{ movie.original_language }}</h5>
+                            <h5 class="card-text">{{ movie.vote_average }}</h5>
                             <span class="px-1">movie</span>
                             <img class="lang_flag" :src="`/src/assets/img/${movie.original_language}.png`"
                                 :alt="movie.original_language">
                         </div>
                     </div>
                 </div>
-                <div class="col-4 py-3" v-for="tvShow in store.tvShows">
+                <div class="col-3 py-3" v-for="tvShow in store.tvShows">
                     <div id="tvshow" class="card bg-dark text-light h-100 p-2"><!-- movie -->
                         <img class="card-img h-100" :src="`https://image.tmdb.org/t/p/w342/${tvShow.poster_path}`" alt="">
                         <div class="card-img-overlay d-flex flex-column justify-content-end">
                             <h3 class="card-text">{{ tvShow.name }}</h3>
-                            <h5 class="card-text">original title: {{ tvShow.original_name }}</h5>
-                            <h5 class="card-text">original language: {{ tvShow.original_language }}</h5>
-                            <h5 class="card-text">average vote: {{ tvShow.vote_average }}</h5>
+                            <h5 class="card-text">{{ tvShow.original_name }}</h5>
+                            <h5 class="card-text">{{ tvShow.original_language }}</h5>
+                            <h5 class="card-text">{{ tvShow.vote_average }}</h5>
                             <span class="px-1">tv show</span>
                             <img class="lang_flag" :src="`/src/assets/img/${tvShow.original_language}.png`"
                                 :alt="tvShow.original_language">
@@ -53,24 +53,20 @@ export default {
 
 <style lang="scss">
 main {
-    /* & .poster_image{
-        object-fit: fill;
-    } */
-
-
 
     & .card-img-overlay {
         display: none!important;
     }
-
     & .card:hover .card-img-overlay {
         display: inherit!important;
     }
-
-    & .card:hover .card-img {
-        filter: opacity(20%);
+    & .card-img{
+        opacity:1;
+        transition: opacity .5s;
     }
-
+    & .card:hover .card-img {
+        opacity:0.2;
+    }
     & .lang_flag {
         aspect-ratio: 1;
         width: 2rem;
