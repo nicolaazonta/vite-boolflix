cb3ff00fd8d144149f1b3cccdd940d596e8fd0c3
nicolaazonta <nicolazonta114@gmail.com>
2023-04-20 16:29:22 +0200

    import and stylize poster images

--- src/components/SiteMain.vue
+++ src/components/SiteMain.vue
@@ -18,32 +18,32 @@ export default {
         <div class="container">
             <div class="row">
                 <div class="col-4 py-3" v-for="movie in store.movies">
-                    <div id="movie" class="card card_special bg-dark text-light h-100 p-2"><!-- movie -->
-                        <div class="card-body">
-                            <h3>title: {{ movie.title }}</h3>
-                            <h5>original title: {{ movie.original_title }}</h5>
-                            <h5>original language: {{ movie.original_language }}</h5>
-                            <h5>average vote: {{ movie.vote_average }}</h5>
+                    <div id="movie" class="card bg-dark text-light h-100 p-2"><!-- movie -->
+                        <img class="card-img h-100" :src="`https://image.tmdb.org/t/p/w342/${movie.poster_path}`" alt="">
+                        <div class="card-img-overlay d-flex flex-column justify-content-end">
+                            <h3 class="card-text">{{ movie.title }}</h3>
+                            <h5 class="card-text">original title: {{ movie.original_title }}</h5>
+                            <h5 class="card-text">original language: {{ movie.original_language }}</h5>
+                            <h5 class="card-text">average vote: {{ movie.vote_average }}</h5>
+                            <span class="px-1">movie</span>
+                            <img class="lang_flag" :src="`/src/assets/img/${movie.original_language}.png`"
+                                :alt="movie.original_language">
                         </div>
-                        <div class="card-footer d-flex justify-content-end align-items-end">
-                            <span class="px-2">movie</span>
-                            <img class="" :src="`/src/assets/img/${movie.original_language}.png`" :alt="movie.original_language">
-                        </div>
-                    </div>                    
+                    </div>
                 </div>
                 <div class="col-4 py-3" v-for="tvShow in store.tvShows">
-                    <div id="tvshow" class="card card_special bg-dark text-light h-100 p-2"><!-- movie -->
-                        <div class="card-body">
-                            <h3>title: {{ tvShow.name }}</h3>
-                            <h5>original title: {{ tvShow.original_name }}</h5>
-                            <h5>original language: {{ tvShow.original_language }}</h5>
-                            <h5>average vote: {{ tvShow.vote_average }}</h5> 
+                    <div id="tvshow" class="card bg-dark text-light h-100 p-2"><!-- movie -->
+                        <img class="card-img h-100" :src="`https://image.tmdb.org/t/p/w342/${tvShow.poster_path}`" alt="">
+                        <div class="card-img-overlay d-flex flex-column justify-content-end">
+                            <h3 class="card-text">{{ tvShow.name }}</h3>
+                            <h5 class="card-text">original title: {{ tvShow.original_name }}</h5>
+                            <h5 class="card-text">original language: {{ tvShow.original_language }}</h5>
+                            <h5 class="card-text">average vote: {{ tvShow.vote_average }}</h5>
+                            <span class="px-1">tv show</span>
+                            <img class="lang_flag" :src="`/src/assets/img/${tvShow.original_language}.png`"
+                                :alt="tvShow.original_language">
                         </div>
-                        <div class="card-footer d-flex justify-content-end align-items-end">
-                            <span class="px-2">tv show</span>
-                            <img class="" :src="`/src/assets/img/${tvShow.original_language}.png`" :alt="tvShow.original_language">
-                        </div>
-                    </div>                    
+                    </div>
                 </div>
             </div>
         </div>
@@ -53,15 +53,27 @@ export default {
 
 <style lang="scss">
 main {
-    & .card-footer{
-        border: none!important;
-        background: none!important;
+    /* & .poster_image{
+        object-fit: fill;
+    } */
+
+
+
+    & .card-img-overlay {
+        display: none!important;
     }
-    & .card-footer img {
-       /*  justify-self: end;
-        align-self: end; */
+
+    & .card:hover .card-img-overlay {
+        display: inherit!important;
+    }
+
+    & .card:hover .card-img {
+        filter: opacity(20%);
+    }
+
+    & .lang_flag {
         aspect-ratio: 1;
-        width: 3rem;
+        width: 2rem;
     }
 }
 </style>
--- src/store.js
+++ src/store.js
@@ -5,6 +5,8 @@ export const store = reactive ({
     lang: null,
     movies: [],
     tvShows: [],
+    
+    api_url_img:'https://image.tmdb.org/t/p/w342/',
     api_url_base_tvShow: 'https://api.themoviedb.org/3/search/tv?api_key=',
     api_url_base_movie: 'https://api.themoviedb.org/3/search/movie?api_key=',
     api_url_key: '4ae8c0c32bcb2919af3d84230c8ddb6e',
